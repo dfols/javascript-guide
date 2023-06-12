4930c1ca8797f1c5075d1172994d97912149636c
dfols <dylanjacobfoley@gmail.com>
2023-06-12 19:35:08 -0400

    updated for in loop

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const array = [1, 2, 3];
 
+// 'for...of' loop - iterates over iterable objects (like arrays or strings),
+// and access the value
+for (const element of array) {
+  console.log(element);
+}
+
+// 'for...in' loop - iterates over the properties of an object
+// (the object keys), usually used for objects
+const person = {
+  name: "John",
+  age: 30,
+  occupation: "Developer",
+};
+
+for (const key in person) {
+  console.log(`Key: ${key}, Value: ${person[key]}`);
+}
+for (const index in array) {
+  console.log(index);
+}
+
+// 'forEach' loop - executes a provided function once for each array element
+array.forEach((element, index) => {
+  console.log(`Element at index ${index} is ${element}`);
+});
